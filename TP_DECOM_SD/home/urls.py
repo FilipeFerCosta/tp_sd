72f2ff39944e90a6c4abb7bcf179791a588a698f
Getúlio Santos <98064959+GetJose@users.noreply.github.com>
2024-09-11 08:20:55 -0300

    fazendo a listagem de todos os artigos junto com a funcionalidade de visualizar um artigo completo

--- TP_DECOM_SD/home/urls.py
+++ TP_DECOM_SD/home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='busca_documentos'),
+    path('/<int:documento_id>/', views.visualizar_documento, name='visualizar_documento'),
+    path('/<int:documento_id>/deletar/', views.deletar_documento, name='deletar_documento'),
+
 ]
+
--- TP_DECOM_SD/home/views.py
+++ TP_DECOM_SD/home/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
+from django.core.paginator import Paginator
+from TP_DECOM_SD.EnvioDoc.models import Documento
 
-# Create your views here.
+def index(request):
+   
+    termo_busca = request.GET.get('q', '')
+
+    
+    if termo_busca:
+        documentos = Documento.objects.filter(
+            Q(titulo__icontains=termo_busca) |
+            Q(autores__icontains=termo_busca) |
+            Q(revista__icontains=termo_busca) |
+            Q(palavras_chave__icontains=termo_busca) |
+            Q(resumo__icontains=termo_busca) |
+            Q(data_publicacao__icontains=termo_busca)
+        )
+    else:
+        documentos = Documento.objects.all()
+
+   
+    ordenacao = request.GET.get('ordenacao', 'titulo')
+    documentos = documentos.order_by(ordenacao)
+
+    paginator = Paginator(documentos, 10)  
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'documentos': page_obj,
+        'page_obj': page_obj,
+        'query': termo_busca,
+        'ordenacao': ordenacao,
+    }
+
+    return render(request, 'busca_documentos.html', context)
+
+def visualizar_documento(request, documento_id):
+    
+    try:
+        documento = Documento.objects.filter(pk=documento_id)
+    except documento.DoesNotExist:
+        raise Http404('Documento Não Existe') # type: ignore
+    context = {
+        'documento' : documento
+        } 
+    return render(request, 'visualizar_documento.html', context)
+
+
+def deletar_documento(request, documento_id):
+    documento = get_object_or_404(Documento, pk=documento_id)
+
+    if request.method == 'POST':
+        documento.delete()
+        messages.success(request, 'Documento excluído com sucesso!')
+        return redirect('busca_documentos')
+    
+    return render(request, 'deletar_documento.html', {'documento': documento})
