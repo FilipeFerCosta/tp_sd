0d1d495d51aa51def5c5ca745645c3225f718585
Getulio <getulio.santos@ufvjm.edu.br>
2024-09-17 09:07:11 -0300

    alterando o nome da url de criacao de documentos para usar na home

--- TP_DECOM_SD/EnvioDoc/urls.py
+++ TP_DECOM_SD/EnvioDoc/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import Atualizar_forms
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='criar_documento'),
     path('editar_documento/<int:pk>/', Atualizar_forms.as_view(), name='editar_artigo'),
 ]
 
