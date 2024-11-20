945f68d0c66eea849f1320f9780a0301bbb2329b
Getulio <getulio.santos@ufvjm.edu.br>
2024-11-20 08:42:33 -0300

    Adicionando arquivo estatic junto com a estilizacao das paginas

--- tp_sd/TP_DECOM_SD/urls.py
+++ tp_sd/TP_DECOM_SD/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('documentos/', include('TP_DECOM_SD.EnvioDoc.urls')),  
     path('api/', include('TP_DECOM_SD.API.urls')),  
     path('accounts/', include('TP_DECOM_SD.accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # Rotas para arquivos estáticos e mídia no modo de desenvolvimento
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
